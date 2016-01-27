57fe597a9781410ea0bf599e85b730cadd1947a4
Joel Karlsson <joeka040@student.liu.se>
2016-01-27 14:04:12 +0100

    First commit

---
+++ Act4Heart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Act4Heart
+//
+//  Created by Joel Karlsson on 2016-01-27.
+//  Copyright © 2016 act4heart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
