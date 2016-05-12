3424ed3f79ee008cb2af5a571ae79e64ed01cc19
Emil <emini901@student.liu.se>
2016-05-12 10:22:44 +0200

    Fixed some stuff

--- Act4Heart/diseaseVC.swift
+++ Act4Heart/diseaseVC.swift
@@ -20,7 +20,7 @@ class diseaseVC: UIViewController {
         self.navBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
         self.navBar.shadowImage = UIImage()
         self.navBar.translucent = true
-        scrollView.contentSize.height = 1350
+        scrollView.contentSize.height = 3300
         
         pushNavigation("Om hjärtinfarkt")
     }
